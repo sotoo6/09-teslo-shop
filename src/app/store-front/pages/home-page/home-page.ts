dddcb32f866bd58149b6b2a614a48fc544402214
luciaSoto <lucias.emerxente1@gmail.com>
2026-03-16 14:59:38 +0100

    Inicio de paginación

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -2,12 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductsService } from '@products/services/products.service';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { RouterLink } from '@angular/router';
+import { Pagination } from "@shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './home-page.html',
 })
 export class HomePage {
